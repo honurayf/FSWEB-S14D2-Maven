dc112deb9ecdee05313d4246632a48935093482c
honurayf <onurayfendal@gmail.com>
2025-08-22 21:24:38 +0300

    S14D2 project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lamp lamp = new Lamp(LampType.NEON,true,100);
+        lamp.turnOn();
+
+        Ceiling ceiling = new Ceiling(2, PaintColor.RED);
+        ceiling.create();
+
+        Bed bed = new Bed("double",2,2,2,2);
+        System.out.println(bed.toString());
+
+        Wall northWall = new Wall("North");
+        Wall eastWall = new Wall("East");
+        Wall westWall = new Wall("West");
+        Wall southWall = new Wall("South");
+
+        Wall[] walls = {northWall,eastWall,westWall,southWall};
+
+        Bedroom bedroom = new Bedroom(northWall,eastWall,southWall,westWall,ceiling,lamp,new Carpet(100,200,PaintColor.WHITE),bed,new Wardrobe(1,2,12),"bedroom1");
+        System.out.println(bedroom.toString());
+        bedroom.createBedroom();
+
+
+
+
     }
 }
---
+++ src/main/java/org/example/model/Bedroom.java
@@ -0,0 +1,46 @@
+package org.example.model;
+
+public class Bedroom extends Room {
+    private String name;
+    private Bed bed;
+    private Wardrobe wardrobe;
+
+    public Bedroom(Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Lamp lamp, Carpet carpet, Bed bed, Wardrobe wardrobe, String name) {
+        super(wall1, wall2, wall3, wall4, ceiling, lamp, carpet);
+        this.bed = bed;
+        this.wardrobe = wardrobe;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Bed getBed() {
+        return bed;
+    }
+
+    public Wardrobe getWardrobe() {
+        return wardrobe;
+    }
+
+    public void createBedroom() {
+        getWall1().create();
+        getWall2().create();
+        getWall3().create();
+        getWall4().create();
+        bed.make();
+        getCarpet().lying();
+        wardrobe.add();
+        getLamp().turnOn();
+    }
+
+    @Override
+    public String toString() {
+        return "Bedroom{" +
+                "name='" + name + '\'' +
+                ", bed=" + bed +
+                ", wardrobe=" + wardrobe +
+                '}';
+    }
+}
